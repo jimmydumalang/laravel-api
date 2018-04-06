f680c801947187c465570540840061cf0cb94775
jimmydumalang <jimmydumalang@gmail.com>
2018-04-07 02:26:55 +0700

    register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
 use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
+use App\Utilities\StatusCode;
+use App\Http\Resources\UserResource;
 
 class RegisterController extends Controller
 {
@@ -31,26 +34,32 @@ class RegisterController extends Controller
     protected $redirectTo = '/home';
 
     /**
+     * Get defined status code
+     *
+     * @var object
+     */
+    protected $statusCode;
+
+    /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(StatusCode $statusCode)
     {
-        $this->middleware('guest');
+        $this->statusCode = $statusCode;
     }
 
     /**
      * Get a validator for an incoming registration request.
      *
-     * @param  array  $data
+     * @param  array $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'phone' => 'required|string|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
     }
@@ -58,15 +67,42 @@ class RegisterController extends Controller
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array  $data
+     * @param  array $data
      * @return \App\User
      */
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
+            'phone' => $data['phone'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function register(Request $request)
+    {
+        $data = $request->all();
+
+        $validator = $this->validator($data);
+
+        if (count($validator->errors()) > 0) {
+            return response()->json([
+                'errors' => $validator->errors(),
+                'response' => [
+                    'status' => $this->statusCode->badRequest(),
+                    'message' => 'BAD REQUEST',
+                ],
+            ]);
+        }
+
+        $register = $this->create($data);
+
+        if ($register) {
+            return new UserResource($register);
+        }
+
+        return response()->json(null)->setStatusCode($this->statusCode->inServerError(), 'Something went wrong');
     }
 }
---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'phone' => $this->phone,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+
+    /**
+     * Get additional data that should be returned with the resource array.
+     *
+     * @param \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function with($request)
+    {
+        return [
+            'response' => [
+                'status' => '204',
+                'message' => 'CREATED',
+            ],
+        ];
+    }
+}
