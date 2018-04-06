f3abce55c1ff570d07a0b89b2aa58cd47b0887d8
jimmydumalang <jimmydumalang@gmail.com>
2018-04-07 03:01:30 +0700

    create orders table

---
+++ database/migrations/2018_04_06_195854_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('reference',50);
+            $table->enum('type', ['PLA','PAL','PLL']);
+            $table->string('origin_name', 50);
+            $table->string('origin_address', 50);
+            $table->string('destination', 50);
+            $table->string('contact', 50);
+            $table->timestamps();
+
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
