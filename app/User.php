eb4fcebebb2e5d2f6c1866cd2f15bbc61556c47c
jimmydumalang <jimmydumalang@gmail.com>
2018-04-06 23:41:29 +0700

    setup laravel passport and user model

--- app/User.php
+++ app/User.php
@@ -4,10 +4,11 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -15,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'phone', 'password',
     ];
 
     /**
@@ -24,6 +25,10 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'name', 'password', 'remember_token',
     ];
+
+    public function findForPassport($username) {
+        return $this->where('phone', $username)->first();
+    }
 }
